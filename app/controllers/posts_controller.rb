486048e89b1fdb70708e4836672467e01e0c6357
Josh Fleming <go@revoltmedia.com>
2016-06-01 19:49:36 -0700

    private function to verify the user is authorized to the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,4 +68,9 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :description)
     end
+
+    def correct_user
+        @post = current_user.posts.find_by(id: params[:id])
+        redirect_to posts_path, notice: "Not authorized to edit this post" if @post.nil?
+    end
 end
